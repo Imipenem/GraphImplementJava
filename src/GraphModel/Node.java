f6f60d7c486bce774925b3bd67dba6bcac8ba365
Imipenem <philipp.ehmele@yahoo.de>
2018-06-17 10:59:30 +0200

    Setup Project Modules

---
+++ src/GraphModel/Node.java
@@ -0,0 +1,4 @@
+package GraphModel;
+
+public class Node {
+}
---
+++ src/com/StartUp/StartUp.java
@@ -0,0 +1,8 @@
+package com.StartUp;
+
+public class StartUp {
+
+    public static void main(String[] args) {
+	System.out.println("HellWorld");
+    }
+}
